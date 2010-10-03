e121d48da94ae6a3a6a5caf3e4b877aaf4d5fbb4
Noah Cornwell <noah0078@gmail.com>
2010-10-02 23:29:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectRepat::Application.config.secret_token = '5b14d90b8ea04af0041df6eb3de0e97328a9bcfbf5be9ac57b15241aed372fc2dae452213f582993ed69f34d636b8199bf98468a616ef30ec11e6e38de101863'
