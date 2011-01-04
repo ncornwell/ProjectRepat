2d07719bfaea7b55e3e06427a1a33471aec76c2c
Noah Cornwell <noah0078@gmail.com>
2011-01-04 00:12:34 -0500

    hmm

--- config/initializers/setup_mail.rb
+++
@@ -1,6 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
